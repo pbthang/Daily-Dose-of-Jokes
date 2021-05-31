a31b1ddb75d1e8c474730856debc54867e4f6415
unknown <bathang02@gmail.com>
2021-05-31 22:42:53 +0800

    fix activity

--- main.js
+++ main.js
@@ -6,9 +6,8 @@ const client = new Discord.Client();
 
 const prefix = "!joke";
 
-client.user.setActivity("!joke help", { type: "PLAYING" });
-
 client.once("ready", () => {
+  client.user.setActivity("!joke help", { type: "PLAYING" });
   console.log(`[Logged in as ${client.user}]`);
 });
 
@@ -51,19 +50,19 @@ client.on("message", (message) => {
 
   if (message.content == `${prefix} help`) {
     message.channel.send(
-      `\`\`\`markdown
-      # Daily-Dose-of-Jokes
-      A discord bot that tells jokes
-      Link: [https://discord.com/api/oauth2/authorize?client_id=848780842683465758&permissions=2148001856&scope=bot]
-      
-      Basic command:
-      - \`!joke\` tell any jokes (including nsfw)
-      - \`!joke [list of categories]\` jokes from a set of categories
-        - List of categories available: Programming, Miscellaneous, Dark, Pun, Spooky, Christmas
-        - E.g: \`!joke dark pun\`
-      - \`!joke safe\` jokes that are safe-for-work and family-friendly
-      
-      API used: [https://sv443.net/jokeapi/v2/]\`\`\``
+      `
+    **Daily-Dose-of-Jokes**
+    A discord bot that tells jokes
+    Link: [https://discord.com/api/oauth2/authorize?client_id=848780842683465758&permissions=2148001856&scope=bot]
+    
+    Basic command:
+    - \`!joke\` tell any jokes (including nsfw)
+    - \`!joke [list of categories]\` jokes from a set of categories
+      - List of categories available: Programming, Miscellaneous, Dark, Pun, Spooky, Christmas
+      - E.g: \`!joke dark pun\`
+    - \`!joke safe\` jokes that are safe-for-work and family-friendly
+    
+    API used: [https://sv443.net/jokeapi/v2/]`
     );
   }
 
