fbdcc365836ccdf0ecf3b04c2c85d4d39c1314e5
unknown <bathang02@gmail.com>
2021-06-01 10:35:30 +0800

    fix

--- main.js
+++ main.js
@@ -55,11 +55,11 @@ client.on("message", (message) => {
     A discord bot that tells jokes
     Link: [https://discord.com/api/oauth2/authorize?client_id=848780842683465758&permissions=2148001856&scope=bot]
     
-    Basic command:
+    **Basic command**:
     - \`!joke\` tell any jokes (including nsfw)
     - \`!joke [list of categories]\` jokes from a set of categories
-      - List of categories available: Programming, Miscellaneous, Dark, Pun, Spooky, Christmas
-      - E.g: \`!joke dark pun\`
+      \t- List of categories available: Programming, Miscellaneous, Dark, Pun, Spooky, Christmas
+      \t- E.g: \`!joke dark pun\`
     - \`!joke safe\` jokes that are safe-for-work and family-friendly
     
     API used: [https://sv443.net/jokeapi/v2/]`
